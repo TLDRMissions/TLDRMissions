eac2a0a13d74f3d560737d522aab580c43dc503b
teelolws <49792789+teelolws@users.noreply.github.com>
2022-04-19 23:19:25 +1200

    Update and rename zhcn.lua to zhtw.lua

--- Translations/zhtw.lua
+++ Translations/zhtw.lua
@@ -1,4 +1,4 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("TLDRMissions", "zhCN")
+local L = LibStub("AceLocale-3.0"):NewLocale("TLDRMissions", "zhTW")
 
 L["Advanced"] = "Advanced >>"
 L["CompleteMissionButtonText"] = "Finish Completed Missions"
