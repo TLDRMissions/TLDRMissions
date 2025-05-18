39b29a0b35b909da5ef5e72d64bd48edf40938b7
Teelo <49792789+teelolws@users.noreply.github.com>
2025-05-18 15:23:24 +1200

    Minor code cleanup

--- Translations/ruRU.lua
+++ Translations/ruRU.lua
@@ -1,5 +1,4 @@
-local addonName = ...
-local addon = _G[addonName]
+local addonName, addon = ...
 local LibStub = addon.LibStub
 local L = LibStub("AceLocale-3.0"):NewLocale("TLDRMissions", "ruRU")
 if not L then return end
